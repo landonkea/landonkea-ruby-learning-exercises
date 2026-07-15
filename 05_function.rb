7f2dc5a4babd0b61de08adc04fa30416ec50e708
LANDON KEA <115629435+landonkea@users.noreply.github.com>
2026-07-15 00:55:46 -0700

    add function: reusable greet logic for multiple names

---
+++ 05_function.rb
@@ -0,0 +1,11 @@
+def greet(name)
+  if name == "LandonTheFirst"
+    puts "Hey, it's you!"
+  else
+    puts "Hello, #{name}!"
+  end
+end
+
+greet("LandonTheFirst")
+greet("LandonTheSecond")
+greet("LandonTheThird")
